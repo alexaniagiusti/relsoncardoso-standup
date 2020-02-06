6507b7a64215d42a989f0ec75396adf607bdf22a
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-06 11:38:20 -0300

    footer e estrura do layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Aqui ficará o rodapé do site</h1>
+  </div>
+</template>
---
+++ src/layouts/SiteLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <Header />
+    <slot></slot>
+    <Footer />
+  </span>
+</template>
+<script>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
