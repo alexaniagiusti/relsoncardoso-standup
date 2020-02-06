67cae58f80abf7938a70441b4dad9280f77d4e21
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-06 11:31:35 -0300

    página home do site

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <span>
-    <Header/>
+    <Header />
+    <Banner v-if="!$vuetify.breakpoint.smAndDown" />
+    <Biografia />
+    <Midias />
   </span>
 </template>
 
 <script>
-import Header from '../components/Header.vue';
+import Header from "../components/Header.vue";
+import Banner from "../components/Banner.vue";
+import Biografia from "../components/Biografia";
+import Midias from "../components/Midias";
 
 export default {
   components: {
     Header,
-  },
+    Banner,
+    Biografia,
+    Midias
+  }
 };
 </script>
+
