d068be7e30e11f4b1becbc300039470a304cf0ed
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-06 11:31:18 -0300

    estrutura inicial do component banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="mx-auto">
+    <v-img
+      src="https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/capa.png?alt=media&token=032a8840-7b0f-4c48-9f05-ba2187597ef5"
+    ></v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src:
+            "https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/capa.png?alt=media&token=032a8840-7b0f-4c48-9f05-ba2187597ef5"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
