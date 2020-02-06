97ab80b35abd886edb873076742ed9c24ee42914
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-06 11:31:00 -0300

    estrutura inicial do component biografia

---
+++ src/components/Biografia.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h1
+      style="font-family: 'Anton', sans-serif; font-size: 19pt; color: #455A64; text-align:center; padding:10px; font-size: 20pt;"
+    >BIOGRAFIA</h1>
+    <div class="container stretch">
+      <v-row>
+        <v-col class="item" cols="12" md="6" xs="12">
+          <v-img
+            src="https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/about-img1.png?alt=media&token=56ca4cb2-4945-4950-9cbc-fab3d3fcd1f9"
+          ></v-img>
+        </v-col>
+        <v-col class="item" cols="12" md="6" xs="12">
+          <h1
+            style="font-family: 'Anton', sans-serif; font-size: 19pt; color: #455A64; text-align:center"
+          >Como comecei?</h1>
+          <p style="text-align: justify; padding: 5px; font-family: 'Noto Sans TC', sans-serif;">
+            Iniciou a sua trajetória no Stand Up em abril de 2019 em um evento denominado Circuito cultural,
+            de lá pra cá não parou mais. Fez a abertura do segundo show de Hulk Pão no teatro das bacabeiras,
+            fez diversos eventos cooporativos, lotou uma casa de shows em Santana juntamente com o grupo Liga
+            da Comédia, onde é um dos fundadores e dividiu por duas noites o palco do teatro das Bacabeiras com
+            os integrantes do grupo de comédia mais badalado de Belém do Pará, o panelinha 2.0.
+          </p>
+          <p style="text-align: justify; padding: 5px; font-family: 'Noto Sans TC', sans-serif;">
+            Relson cardoso sempre escreveu textos relatando situações do cotidiano, sempre carregados de muito bom
+            humor, nem imaginava que estava produzindo um material na estrutura do Stand Up Comedy. Quando se interessou
+            pelo assunto e começou a pesquisar a parte técnica desse seguimento da comédia, percebeu que aqueles seus
+            textos que sempre escrevia desde muito tempo nada mais eram do que criações no modelo Stand Up.
+          </p>
+          <p style="text-align: justify; padding: 5px; font-family: 'Noto Sans TC', sans-serif;">
+            Após uma conversa que teve com o amigo Joabe Silva em sua residência, decidiu que deveria arrumar uma maneira
+            de mostrar para as outras pessoas os seus textos através de uma apresentação, foi aí que tiveram a ideia
+            de formar o grupo humoristico Liga da Comédia, coincidentemente nessa mesma época, iria acontecer em Santana
+            uma espécie de festival de talentos, o grupo foi convidado pelo organizador para fazer uma apresentação
+            nesse dia, apesar do pouco tempo que tiveram para se preparar, a estreia foi um sucesso de criticas.
+          </p>
+          <p style="text-align: justify; padding: 5px; font-family: 'Noto Sans TC', sans-serif;">
+            Alguns meses depois o grupo teve um evento na principal casa de shows de Santana, com os imgressos esgotados,
+            o Liga da Comédia realizou um grande show, com textos 100% autorais entrerteram a platéia durante toda a
+            apresentação. Mais uma vez os elogios se fizeram presentes, logo após o evento, Relson Cardoso decide sair
+            do grupo para focar em projetos solos, mas sempre contando com o apoio dos outros integrantes.
+          </p>
+          <p style="text-align: justify; padding: 5px; font-family: 'Noto Sans TC', sans-serif;">
+            Os convites para apresentações continuaram sergindo e percebeu-se a necessidade de criar um show
+            mais bem elaborado e com mais tempo para apresentar seus textos ao público, após alguns meses escrevendo
+            e produzindo material inédito, Relson Cardoso anuncia para o inicio de 2020 o seu show solo intitulado, "Como Eu Vejo O Mundo".
+          </p>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Flex Container */
+.container {
+  margin: 0 auto;
+  display: flex;
+}
+
+.stretch {
+  align-items: stretch;
+}
+
+/* Flex Item */
+.item {
+  flex: 1;
+  margin: 10px;
+  padding: 0 10px;
+  text-align: center;
+  font-size: 12pt;
+}
+</style>
