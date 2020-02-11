c5282271f57979afb9f0a3fc552ad3135afac928
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-11 10:05:36 -0300

    página home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <Banner v-if="!$vuetify.breakpoint.smAndDown" />
     <Biografia />
     <Midias />
+    <Footer />
   </span>
 </template>
 
@@ -12,13 +13,15 @@ import Header from "../components/Header.vue";
 import Banner from "../components/Banner.vue";
 import Biografia from "../components/Biografia";
 import Midias from "../components/Midias";
+import Footer from "../components/Footer";
 
 export default {
   components: {
     Header,
     Banner,
     Biografia,
-    Midias
+    Midias,
+    Footer
   }
 };
 </script>
