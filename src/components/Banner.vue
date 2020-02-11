f9bd0f82d685cb25539b778d47017c5d696a9212
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-11 10:04:26 -0300

    component onde é exibido o carousel

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,9 +1,13 @@
 <template>
-  <v-card class="mx-auto">
-    <v-img
-      src="https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/capa.png?alt=media&token=032a8840-7b0f-4c48-9f05-ba2187597ef5"
-    ></v-img>
-  </v-card>
+  <v-carousel height="670px" app show-arrows-on-hover>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
@@ -13,16 +17,19 @@ export default {
       items: [
         {
           src:
-            "https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/capa.png?alt=media&token=032a8840-7b0f-4c48-9f05-ba2187597ef5"
+            "https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/_SMP7473.jpg?alt=media&token=633035e3-e1ba-4f7f-8c94-f4e223b7cea7"
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+          src:
+            "https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/_SMP7389.jpg?alt=media&token=a3a0e688-eed9-4517-b2d2-4c6357046c6d"
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+          src:
+            "https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/_SMP7395.jpg?alt=media&token=6ce9de95-67c8-4aec-ab57-fd897d6571e5"
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+          src:
+            "https://firebasestorage.googleapis.com/v0/b/relson-cardoso.appspot.com/o/_SMP7479.jpg?alt=media&token=9f5f31e5-8bf7-4e27-98cd-7b6f0a877eec"
         }
       ]
     };
