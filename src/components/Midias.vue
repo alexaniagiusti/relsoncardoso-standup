93905a3e4542923cec1860d61b0d8f1b291caf29
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-11 10:02:02 -0300

    tela onde serão exibidos fotos e videos

--- src/components/Midias.vue
+++ src/components/Midias.vue
@@ -1,28 +1,72 @@
 <template>
-  <v-row>
-    <v-col cols="12" md="12">
-      <v-card>
-        <v-container fluid>
-          <v-row>
-            <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
-              <v-card flat tile class="d-flex">
-                <v-img
-                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-row class="fill-height ma-0" align="center" justify="center">
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div id="midias">
+    <!-- Primeiro Título  -->
+    <h1
+      style="font-family: 'Asap', sans-serif; text-align:center; color: #fff; padding: 10px;"
+    >MÍDIAS</h1>
+    <!-- Incio da primeira row -->
+    <v-row class="container stretch">
+      <v-col cols="4" class="item">
+        <v-card hover max-width="344" class="mx-auto">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">TODOS</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="4" class="item">
+        <v-card hover max-width="344" class="mx-auto">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">FOTOS</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="4" class="item">
+        <v-card hover max-width="344" class="mx-auto">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">VÍDEOS</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- Fechamento da Primeira row -->
+
+    <!-- Segundo título -->
+  </div>
 </template>
+
+
+<style scoped>
+#midias {
+  background-image: url(../assets/img/6828.jpg);
+}
+
+.container {
+  margin: 0 auto;
+  display: flex;
+}
+
+.stretch {
+  align-items: stretch;
+}
+
+/* Flex Item */
+.item {
+  flex: 1;
+  margin: 10px;
+  padding: 0 10px;
+  text-align: center;
+  font-size: 12pt;
+}
+</style>
