8fc7fef9a28d96946d16e4a975e87a9f384ec92f
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-25 18:41:11 -0300

    update no component e integração com backend

--- src/components/Midias.vue
+++ src/components/Midias.vue
@@ -4,44 +4,48 @@
     <h1
       style="font-family: 'Asap', sans-serif; text-align:center; color: #fff; padding: 10px;"
     >MÍDIAS</h1>
-    <!-- Incio da primeira row -->
     <v-row class="container stretch">
-      <v-col cols="4" class="item">
+      <v-col v-for="item in Galeria" :key="item._id" cols="12" md="4" xs="12" class="item">
         <v-card hover max-width="344" class="mx-auto">
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">TODOS</v-list-item-title>
+              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">
+                <h5 class="mt-2">{{ item.titulo }}</h5>
+              </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
-        </v-card>
-      </v-col>
-      <v-col cols="4" class="item">
-        <v-card hover max-width="344" class="mx-auto">
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">FOTOS</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
-        </v-card>
-      </v-col>
-      <v-col cols="4" class="item">
-        <v-card hover max-width="344" class="mx-auto">
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="headline" style="font-family: 'Anton', sans-serif;">VÍDEOS</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+          <v-img :src="item.imagem_capa" height="194"></v-img>
         </v-card>
       </v-col>
     </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      Galeria: []
+    };
+  },
+  methods: {
+    getGaleria() {
+      this.$http
+        .get("/galeria")
+        .then(res => {
+          this.Galeria = res.data;
+        })
+        .catch(erro => {
+          console.log("caiu no erro");
+        });
+    }
+  },
+  created() {
+    this.getGaleria();
+  }
+};
+</script>>
 
 
 <style scoped>
@@ -60,9 +64,6 @@
 
 /* Flex Item */
 .item {
-  flex: 1;
-  margin: 10px;
-  padding: 0 10px;
   text-align: center;
   font-size: 12pt;
 }
