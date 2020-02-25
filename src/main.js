5884577d78e7487680fd9a2be3bc52005c34fe66
Alexânia Giusti <alexaniagiusti@hotmail.com>
2020-02-25 18:40:40 -0300

    integração com backend local

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000'
+})
 
 new Vue({
   router,
